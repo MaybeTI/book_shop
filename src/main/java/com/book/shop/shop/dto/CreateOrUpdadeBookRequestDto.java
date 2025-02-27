3ce63827c1626d1feeaf2948234f88c8ae83879b
Bohdan Lysachenko <lysachenko111@ukr.net>
2025-02-27 13:12:00 +0200

    add JpaRepository

--- src/main/java/com/book/shop/shop/dto/CreateOrUpdadeBookRequestDto.java
+++ src/main/java/com/book/shop/shop/dto/CreateOrUpdadeBookRequestDto.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
-public class CreateBookRequestDto {
+public class CreateOrUpdadeBookRequestDto {
     private String title;
     private String author;
     private String isbn;
