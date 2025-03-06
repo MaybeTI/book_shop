778512dc0e99d658cf1aaf09ceb4df9de1eca2f2
Bohdan Lysachenko <lysachenko111@ukr.net>
2025-03-06 22:14:20 +0200

    Added pagination and openapi service

--- src/main/java/com/book/shop/dto/BookSearchParametersDto.java
+++ src/main/java/com/book/shop/dto/BookSearchParametersDto.java
@@ -1,4 +1,14 @@
 package com.book.shop.dto;
 
-public record BookSearchParametersDto(String[] titles, String[] authors) {
-}
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "DTO for searching books by title or author")
+public record BookSearchParametersDto(
+        @Schema(description = "Array of book titles to search for",
+                example = "[\"Effective Java\", \"Clean Code\"]")
+        String[] titles,
+
+        @Schema(description = "Array of book authors to search for",
+                example = "[\"Joshua Bloch\", \"Robert C. Martin\"]")
+        String[] authors
+) {}
--- src/main/java/com/book/shop/dto/CreateOrUpdateBookRequestDto.java
+++ src/main/java/com/book/shop/dto/CreateOrUpdateBookRequestDto.java
@@ -1,23 +1,39 @@
 package com.book.shop.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
+@Schema(description = "DTO for creating or updating a book")
 public class CreateOrUpdateBookRequestDto {
+
     @NotNull
+    @Schema(description = "Title of the book", example = "The Pragmatic Programmer")
     private String title;
+
     @NotNull
+    @Schema(description = "Author of the book", example = "Andy Hunt and Dave Thomas")
     private String author;
+
     @NotNull
+    @Schema(description = "ISBN number of the book", example = "978-0135957059")
     private String isbn;
+
     @NotNull
     @Positive
+    @Schema(description = "Price of the book", example = "39.99")
     private BigDecimal price;
+
     @NotNull
+    @Schema(description = "Description of the book",
+            example = "A guide to becoming a better software developer")
     private String description;
+
     @NotNull
+    @Schema(description = "URL of the book cover image",
+            example = "https://example.com/pragmatic.jpg")
     private String coverImage;
 }
