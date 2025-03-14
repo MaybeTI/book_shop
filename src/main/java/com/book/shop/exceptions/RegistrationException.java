c9e0b1c92c47d94a63980cde1122810c0d4c1cd8
Bohdan Lysachenko <lysachenko111@ukr.net>
2025-03-14 15:08:27 +0200

    Added registration

---
+++ src/main/java/com/book/shop/exceptions/RegistrationException.java
@@ -0,0 +1,7 @@
+package com.book.shop.exceptions;
+
+public class RegistrationException extends RuntimeException {
+    public RegistrationException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/book/shop/service/UserServiceImpl.java
@@ -0,0 +1,24 @@
+package com.book.shop.service;
+
+import com.book.shop.dto.UserRegistrationRequestDto;
+import com.book.shop.dto.UserResponseDto;
+import com.book.shop.exceptions.RegistrationException;
+import com.book.shop.mapper.UserMapper;
+import com.book.shop.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    @Override
+    public UserResponseDto createUser(UserRegistrationRequestDto requestDto) {
+        if (userRepository.findByEmail(requestDto.getEmail()).isPresent()) {
+            throw new RegistrationException("Email is already in use");
+        }
+        return userMapper.toDto(userRepository.save(userMapper.toModel(requestDto)));
+    }
+}
