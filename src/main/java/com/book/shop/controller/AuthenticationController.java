0db46280b2cc653ccaf33623a01590bc9e304983
Bohdan Lysachenko <lysachenko111@ukr.net>
2025-04-04 14:08:58 +0300

    Add JWT-token

--- src/main/java/com/book/shop/controller/AuthenticationController.java
+++ src/main/java/com/book/shop/controller/AuthenticationController.java
@@ -1,8 +1,11 @@
 package com.book.shop.controller;
 
+import com.book.shop.dto.UserLoginRequestDto;
+import com.book.shop.dto.UserLoginResponseDto;
 import com.book.shop.dto.UserRegistrationRequestDto;
 import com.book.shop.dto.UserResponseDto;
 import com.book.shop.exceptions.RegistrationException;
+import com.book.shop.security.AuthenticationService;
 import com.book.shop.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -16,11 +19,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthenticationController {
     private final UserService userService;
+    private final AuthenticationService authenticationService;
 
     @PostMapping("/registration")
     public UserResponseDto register(
             @RequestBody @Valid UserRegistrationRequestDto requestDto
     ) throws RegistrationException {
         return userService.createUser(requestDto);
+    }
+
+    @PostMapping("/login")
+    public UserLoginResponseDto login(@RequestBody @Valid UserLoginRequestDto request) {
+        return authenticationService.authenticate(request);
     }
 }
---
+++ src/main/java/com/book/shop/dto/UserLoginResponseDto.java
@@ -0,0 +1,4 @@
+package com.book.shop.dto;
+
+public record UserLoginResponseDto(String token) {
+}
